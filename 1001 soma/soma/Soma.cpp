c4362184453ec90bb71ede324aabf190051744b7
Davi <davisouzamachado@outlook.com>
2018-09-26 19:31:27 -0300

    1001 Soma

---
+++ 1001 soma/soma/Soma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 0, b = 0, x = 0;
+	cout << "Soma entre dois valores " << endl;
+	cin >> a;
+	cin >> b;
+	x = a + b;
+	cout << "x = " << x << endl;
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
