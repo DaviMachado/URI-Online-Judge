210b64c9863ba5d9a3234c61d2995ce318d00edc
Davi <davisouzamachado@outlook.com>
2018-10-09 18:27:30 -0300

    area

---
+++ 1002 area do circulo/area do circulo/Area do circulo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double r = 0, a = 0;
+	cout << "Area do Circulo" << endl;
+	cin >> r;
+	a = r * r * 3.14159;
+	cout << "A = " << a << endl;
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
