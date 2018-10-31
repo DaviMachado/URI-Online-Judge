55badfcd25363172c3cc7f84af5bce010f23229c
Davi <davisouzamachado@outlook.com>
2018-10-31 20:08:47 -0300

    calculo do produto

---
+++ 1004 cálculo do produto/1004 cálculo do produto/Cálculo do produto .cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 0, b = 0, prod = 0;
+	cin >> a;
+	cin >> b;
+	prod = a*b;
+	cout << "PROD = " << prod << endl;
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
