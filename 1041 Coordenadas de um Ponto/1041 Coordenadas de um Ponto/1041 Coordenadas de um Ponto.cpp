6549ef7d8d00771d6ca066dd9b49dc6ab43bf493
Davi Rodrigues <davisouzamachado@outlook.com>
2019-03-29 07:17:11 -0300

    Debug

---
+++ 1041 Coordenadas de um Ponto/1041 Coordenadas de um Ponto/1041 Coordenadas de um Ponto.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float x = 0, y = 0;
+	cin >> x >> y;
+
+	if (x == 0)
+	{
+		if (y == 0)
+		{
+			cout << "Origem" << endl;
+		}
+	}
+
+	if (x == 0)
+	{
+		if (y > 0)
+		{
+			cout << "Eixo Y" << endl;
+		}
+	}
+	if (x == 0)
+	{
+		if (y < 0)
+		{
+			cout << "Eixo Y" << endl;
+		}
+	}
+
+	if (y == 0)
+	{
+		if (x > 0)
+		{
+			cout << "Eixo X" << endl;
+		}
+	}
+	if (y == 0)
+	{
+		if (x < 0)
+		{
+			cout << "Eixo X" << endl;
+		}
+	}
+
+	if (x > 0)
+	{
+		if (y > 0)
+		{
+			cout << "Q1" << endl;
+		}
+	}
+
+	if (x < 0)
+	{
+		if (y > 0)
+		{
+			cout << "Q2" << endl;
+		}
+	}
+
+	if (x < 0)
+	{
+		if (y < 0)
+		{
+			cout << "Q3" << endl;
+		}
+	}
+
+	if (x > 0)
+	{
+		if (y < 0)
+		{
+			cout << "Q4" << endl;
+		}
+	}
+
+	system("PAUSE");
+	return 0;
+}
